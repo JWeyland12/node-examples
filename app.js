6976976fb0e1cf2dc5bc1872576ba55778884630
Jason Weyland <jason.weyland13@gmail.com>
2020-08-04 13:17:09 -0400

    Example JavaScript Program

---
+++ app.js
@@ -0,0 +1,20 @@
+const rect = {
+  perimeter: (x, y) => 2 * (x+y),
+  area: (x, y) => x * y
+};
+
+function solvRect(l, w) {
+  console.log(`Solving for rectangle dimensions: ${l}, ${w}`);
+
+  if (l <= 0 || w <= 0) {
+    console.log(`Rectangle dimensions must be greater than zero, Received: ${l} ,${w}`)
+  } else {
+    console.log(`Area of rectangle: ${rect.area(l,w)}`);
+    console.log(`Perimeter of rectangle: ${rect.perimeter(l,w)}`)
+  }
+}
+
+solvRect(2,4);
+solvRect(3,5);
+solvRect(0,5);
+solvRect(5,-3);
